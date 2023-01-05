e9eec4c8768c988e06964919960e7c4e7f47a454
rlagkswn00 <hjkim2902@naver.com>
2023-01-06 00:36:12 +0900

    [C++] BOJ 25897 Commit

---
+++ C++Algorithm/2587.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int input[5], sum = 0;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> input[i];
+        sum += input[i];
+    }
+
+    sort(input, input + 5);
+
+    cout << sum / 5 << "\n";
+    cout << input[2] << "\n";
+}
