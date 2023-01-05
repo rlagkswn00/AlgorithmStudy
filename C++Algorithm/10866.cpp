fb12b23541e9fd93a5fec10e4f5db0c3676577df
rlagkswn00 <hjkim2902@naver.com>
2023-01-05 16:29:03 +0900

    [C++] BOJ 10866 commit

---
+++ C++Algorithm/10866.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    deque<int> dq;
+    int n;
+    cin >> n;
+    string s;
+    while (n--)
+    {
+        cin >> s;
+        if (s == "push_front")
+        {
+            int num;
+            cin >> num;
+            dq.push_front(num);
+        }
+        else if (s == "push_back")
+        {
+            int num;
+            cin >> num;
+            dq.push_back(num);
+        }
+        else if (s == "pop_front")
+        {
+            if (!dq.empty())
+            {
+                cout << dq.front() << "\n";
+                dq.pop_front();
+            }
+            else
+                cout << -1 << "\n";
+        }
+        else if (s == "pop_back")
+        {
+            if (!dq.empty())
+            {
+                cout << dq.back() << "\n";
+                dq.pop_back();
+            }
+            else
+                cout << -1 << "\n";
+        }
+        else if (s == "size")
+        {
+            cout << dq.size() << "\n";
+        }
+        else if (s == "empty")
+        {
+            cout << dq.empty() << "\n";
+        }
+        else if (s == "front")
+        {
+            if (!dq.empty())
+            {
+                cout << dq.front() << "\n";
+            }
+            else
+                cout << -1 << "\n";
+        }
+        else
+        {
+            if (!dq.empty())
+            {
+                cout << dq.back() << "\n";
+            }
+            else
+                cout << -1 << "\n";
+        }
+    }
+}
