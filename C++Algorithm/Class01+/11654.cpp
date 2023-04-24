3c9d0f674c9bbf0ead0aa4fe74c40d61f51465d8
rlagkswn00 <hjkim2902@naver.com>
2023-04-24 15:24:45 +0900

    [C++] BOJ 11654 commit

---
+++ C++Algorithm/Class01+/11654.cpp
@@ -0,0 +1,19 @@
+/*
+Title: BOJ 2920 음계
+Theory : 구현
+Date: 23.04.24
+*/
+
+#include <iostream>
+using namespace std;
+char c;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> c;
+
+    cout << (int)c;
+}
