7245a1889e7f0625ec050034f3b11279a7a7e849
rlagkswn00 <hjkim2902@naver.com>
2023-04-26 14:54:21 +0900

    [C++] BOJ 1085 commit

---
+++ C++Algorithm/Class02/1085.cpp
@@ -0,0 +1,18 @@
+/*
+Title: BOJ 1085 직사각형 탈출
+Theory :
+Date: 23.04.26
+*/
+#include <iostream>
+#include <utility>
+#include <cmath>
+using namespace std;
+pair<int, int> xy, wh;
+int main(void)
+{
+    cin >> xy.first >> xy.second >> wh.first >> wh.second;
+    int val1 = min(xy.first, xy.second);
+    int val2 = min(wh.first - xy.first, wh.second - xy.second);
+    cout << min(val1, val2);
+    return 0;
+}
