cfeabe96b40d896df3766c0f96550ab225919714
rlagkswn00 <hjkim2902@naver.com>
2023-05-03 13:52:47 +0900

    [C++] BOJ 4153 commit

---
+++ C++Algorithm/Class02/Class02+/4153.cpp
@@ -0,0 +1,23 @@
+/*
+Title: BOJ 4153 직각삼각형
+Theory :
+Date: 23.05.03
+*/
+#include <iostream>
+
+using namespace std;
+int a, b, c;
+int main(void)
+{
+    while (true)
+    {
+        cin >> a >> b >> c;
+        if (a == 0 && b == 0 && c == 0)
+            break;
+        if (((a * a) + (b * b)) == (c * c) || ((a * a) + (c * c)) == (b * b) || ((b * b) + (c * c)) == (a * a))
+            cout << "right\n";
+        else
+            cout << "wrong\n";
+    }
+    return 0;
+}
