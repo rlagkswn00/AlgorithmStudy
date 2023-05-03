16ad29e7250cbbe462d2b044b7356e10a1ff5f90
rlagkswn00 <hjkim2902@naver.com>
2023-05-03 13:47:24 +0900

    [C++] BOJ 2798 commit

---
+++ C++Algorithm/Class02/Class02+/2798.cpp
@@ -0,0 +1,47 @@
+/*
+Title: BOJ 2798 블랙잭
+Theory :
+Date: 23.05.03
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+vector<int> vec;
+int main(void)
+{
+    cin >> n >> m;
+    int sum = 0;
+    int maxSum = 0;
+    int tmp = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> tmp;
+        vec.push_back(tmp);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        sum += vec[i];
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+                continue;
+            sum += vec[j];
+            for (int k = 0; k < n; k++)
+            {
+                if (j == k || k == i)
+                    continue;
+                sum += vec[k];
+                if (sum > maxSum && sum <= m)
+                    maxSum = sum;
+                sum -= vec[k];
+            }
+            sum -= vec[j];
+        }
+        sum -= vec[i];
+    }
+    cout << maxSum;
+    return 0;
+}
