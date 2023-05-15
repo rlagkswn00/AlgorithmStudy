dea708f7d69182d1f1ce40866502964652b061af
rlagkswn00 <hjkim2902@naver.com>
2023-05-15 22:38:29 +0900

    [JOIN] 5월 식품들의 총매출 조회하기

--- Programmers_SQL/JOIN/5월 식품둘의 총매출 조회하기.sql
+++ Programmers_SQL/JOIN/5월 식품둘의 총매출 조회하기.sql
@@ -1,10 +1,10 @@
 --
 -- Title: 5월 식품들의 총매출 조회하기 
--- Date: 23.05.10
+-- Date: 23.05.15
 --
 SELECT a.PRODUCT_ID, PRODUCT_NAME,  sum(AMOUNT*PRICE) as TOTAL_SALES
-FROM FOOD_PRODUCT as a inner join FOOD_ORDER as b
+FROM FOOD_PRODUCT as a join FOOD_ORDER as b
 on a.PRODUCT_ID = b.PRODUCT_ID
+where b.PRODUCE_DATE like '2022-05%'
 group by PRODUCT_ID
-where PRODUCE_DATE like '2022-05%'
-order by total desc, a.PRODUCT_ID desc
+order by TOTAL_SALES desc, a.PRODUCT_ID asc
