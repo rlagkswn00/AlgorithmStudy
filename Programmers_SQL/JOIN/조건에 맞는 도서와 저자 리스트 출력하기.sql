6a99f7bdb39b87ead618ceb5ddf01d3308ab85cd
rlagkswn00 <hjkim2902@naver.com>
2023-05-16 16:14:37 +0900

    [JOIN] 조건에 맞는 도서와 저자 리스트 출력하기

---
+++ Programmers_SQL/JOIN/조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -0,0 +1,10 @@
+--
+-- Title: 조건에 맞는 도서와 저자 리스트 출력하기 
+-- Date: 23.05.16
+--
+
+select a.BOOK_ID, b.AUTHOR_NAME, a.PUBLISHED_DATE
+from BOOK as a join AUTHOR as b
+on a.AUTHOR_ID = b.AUTHOR_ID
+where a.CATEGORY = '경제'
+order by a.PUBLISHED_DATE asc
