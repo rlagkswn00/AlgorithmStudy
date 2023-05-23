4be3888a1720105a31596282b12576bc758a6b45
rlagkswn00 <hjkim2902@naver.com>
2023-05-23 19:46:17 +0900

    [SELECET] 조건에 맞는 도서 리스트 출력하기 commit

--- Programmers_SQL/SELECT/조건에 맞는 도서 리스트 출력하기.sql
+++ Programmers_SQL/SELECT/조건에 맞는 도서 리스트 출력하기.sql
@@ -4,6 +4,6 @@
 --
 select BOOK_ID, DATE_FORMAT(PUBLISHED_DATE,'%Y-%m-%d') as PUBLISHED_DATE
 from BOOK
-where PUBLISHED_DATE like '202%'
+where PUBLISHED_DATE like '2021%'
 and CATEGORY = '인문'
 order by PUBLISHED_DATE asc
