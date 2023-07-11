0fe0eeb030916da7cd2bf5d0617ba72f20969f68
Hanju Kim <hjkim2902@gmail.com>
2023-07-11 14:33:13 +0900

    [JAVA] basic Queue Commit

---
+++ JavaAlgorithm/src/queue/QueueTest.java
@@ -0,0 +1,18 @@
+package queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueTest {
+	public static void main(String[] args) {
+		Queue<Integer> q = new LinkedList<>();
+		
+		q.add(100);
+		q.add(200);
+		q.add(300);
+		q.add(400);
+		q.add(500);
+		System.out.println(q.peek());
+		
+	}
+}
