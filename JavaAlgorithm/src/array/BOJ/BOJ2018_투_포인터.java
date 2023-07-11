1411fcbc3426a27fe64cd814a79d367564726717
Hanju Kim <hjkim2902@gmail.com>
2023-07-11 13:19:53 +0900

    [JAVA] BOJ 2018 commit

--- JavaAlgorithm/src/array/BOJ/BOJ2018_투_포인터.java
+++ JavaAlgorithm/src/array/BOJ/BOJ2018_투_포인터.java
@@ -3,35 +3,38 @@ package array.BOJ;
 import java.util.*;
 import java.io.*;
 
-public class BOJ2018_투_포인터 {
+public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 
-	static int total(int front,int back) {
+	static int total(int front, int back) {
 		int answer = 0;
-		for(int i = front ; i < back + 1 ; i++)
+		for (int i = front; i < back + 1; i++)
 			answer += i;
 		return answer;
 	}
+
 	public static void main(String[] args) throws IOException {
 		String input = br.readLine();
 		st = new StringTokenizer(input);
 		int n = Integer.parseInt(st.nextToken());
 
 		int start = 1;
-		int end = 2;
+		int end = 1;
 		int cnt = 0;
-		while(end <= n) {			
-			if(total(start,end) == n) {
+		int sum = 0;
+		while (end <= n) {
+			sum = total(start, end);
+			if (sum== n) {
 				cnt++;
 				start++;
 				end++;
-			}else if(total(start,end) < n) {
+			} else if (sum < n) {
 				end++;
-			}else {
+			} else {
 				start++;
 			}
-			
+
 		}
 		System.out.println(cnt);
 	}
