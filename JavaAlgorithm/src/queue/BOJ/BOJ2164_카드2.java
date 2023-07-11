63cebe107be12f82674aa0530b2c8bbaa21a8d9c
Hanju Kim <hjkim2902@gmail.com>
2023-07-11 14:33:25 +0900

    [JAVA] BOJ 2164 Commit

---
+++ JavaAlgorithm/src/queue/BOJ/BOJ2164_카드2.java
@@ -0,0 +1,23 @@
+package queue.BOJ;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Queue<Integer> q = new LinkedList<>();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i = 0 ; i < n ; i++)
+			q.add(i+1);
+		
+		while(q.size() != 1) {
+			q.remove();
+			int tmp = q.remove();
+			q.add(tmp);
+		}
+		System.out.println(q.peek());
+	}
+}
