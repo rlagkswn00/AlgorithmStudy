9c66292fcb64215cc7b9735122ed56a2cb03461e
Hanju Kim <hjkim2902@gmail.com>
2023-07-12 16:18:31 +0900

    [JAVA] BOJ 2493 재풀이 Commit

--- JavaAlgorithm/src/stack/BOJ/BOJ2493_탑.java
+++ JavaAlgorithm/src/stack/BOJ/BOJ2493_탑.java
@@ -6,30 +6,32 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BOJ2493_탑 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		Stack<int[]> st1 = new Stack<>();
 		StringBuilder sb = new StringBuilder();
+		Stack<int[]> stack = new Stack<>();
 		int n = Integer.parseInt(st.nextToken());
-		
-		st = new StringTokenizer(br.readLine());
-		
-		for (int i = 1; i < n + 1; i++) {
-			int top = Integer.parseInt(st.nextToken());
-			while(!st1.isEmpty()) {
-				if(st1.peek()[1] >= top) {
-					sb.append(st1.peek()[0] + " ");
-					break;
-				}
-				st1.pop();
-			}
-			if(st1.isEmpty())
-				sb.append(0 + " ");
-			st1.push(new int[] {i,top});
-		}
-		System.out.print(sb);
 
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 1; i < n + 1; i++) {
+			int inputVal = Integer.parseInt(st.nextToken());
+			if (!stack.isEmpty())
+				while (stack.peek()[1] < inputVal) {
+					stack.pop();
+					if (stack.isEmpty())
+						break;
+				}
+			
+			if (stack.isEmpty()) {
+				sb.append(0 + " ");
+				stack.push(new int[] { i, inputVal });
+			} else
+				sb.append(stack.peek()[0] + " ");
+			stack.push(new int[] { i, inputVal });
+		}
+		System.out.println(sb);
 	}
 }
