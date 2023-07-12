a29ba8be6dff7490ecedad3653732c41f3148d69
Hanju Kim <hjkim2902@gmail.com>
2023-07-12 13:12:37 +0900

    [JAVA] Jungol 1146 Commit

---
+++ JavaAlgorithm/src/sorting/JUNGOL/JUNG1146_선택정렬.java
@@ -0,0 +1,40 @@
+package sorting.JUNGOL;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class JUNG1146_선택정렬 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		int n = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+
+		for (int i = 0; i < n-1; i++) {
+			int minIdx = i;
+			for (int j = i+1; j < n; j++)
+				if (arr[j] < arr[minIdx])
+					minIdx = j;
+
+			int tmp = arr[i];
+			arr[i] = arr[minIdx];
+			arr[minIdx] = tmp;
+			System.out.println(minIdx + " " + i + " ");
+			for(int j = 0 ; j < n ; j++)
+				sb.append(arr[j] + " ");
+			System.out.println(sb.toString().strip());
+			sb = new StringBuilder();
+
+
+		}
+	}
+}
