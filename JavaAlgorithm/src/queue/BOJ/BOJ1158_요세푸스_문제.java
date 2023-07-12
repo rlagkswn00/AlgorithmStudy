a62225b2004c0c0873dbded02de22cdb5a709a51
Hanju Kim <hjkim2902@gmail.com>
2023-07-12 14:39:29 +0900

    [JAVA] BOJ 1158 재풀이 Commit

--- JavaAlgorithm/src/queue/BOJ/BOJ1158_요세푸스_문제.java
+++ JavaAlgorithm/src/queue/BOJ/BOJ1158_요세푸스_문제.java
@@ -10,37 +10,33 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException{
 		Queue<Integer> q = new LinkedList<>();
-		Queue<Integer> resultQ = new LinkedList<>();
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+		StringBuilder sb = new StringBuilder();
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
 		
 		for(int i = 1 ; i < n+1 ;i++)
 			q.add(i);
 		
-		int cnt = 0;
+		int cnt = 1;
+		sb.append("<");
 		while(!q.isEmpty()) {
-			if(cnt == k-1) {
-				//pop
-				resultQ.add(q.remove());
+//			System.out.println(q);
+			if(cnt % k != 0) {
+				cnt++;
+				q.add(q.remove());
 			}else {
-				//re push
-				int tmp = q.remove();
-				q.add(tmp);
-			}
-			
-			cnt = (cnt+1) % k;
-		}
-		System.out.print("<");
-		while(!resultQ.isEmpty()) {
-			System.out.print(resultQ.poll());
-			if(!resultQ.isEmpty()) {
-				 System.out.print(", ");
+				cnt = 1;
+				sb.append(q.remove());
+				if(!q.isEmpty())
+					sb.append(", ");
+				else
+					continue;
 			}
 		}
-		System.out.print(">");
+		sb.append(">");
+		System.out.println(sb);
 	}
 }
