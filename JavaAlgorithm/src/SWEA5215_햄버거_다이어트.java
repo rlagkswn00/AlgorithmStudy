f3da0b41b46efe1061bec22ed5b439291b77b036
Han Ju Kim <hjkim2902@gmail.com>
2023-07-15 11:05:10 +0900

    [JAVA] SWEA 5215 Commit

--- JavaAlgorithm/src/SWEA5215_햄버거_다이어트.java
+++ JavaAlgorithm/src/SWEA5215_햄버거_다이어트.java
@@ -9,25 +9,31 @@ public class SWEA5215_햄버거_다이어트 {
 	static int[][] arr;
 	static int N;
 	static int L;
-	static int maxScore = 0;
+	static int maxScore;
 
+	//탐색 간 칼로리, 맛 정보 저장
 	static void dfs(int n, int cal, int score) {
 		if (n == N) {
+			//칼로리가 기준 L 보다 작을경우
+			// 점수가 이전까지의 최대 점수보다 클 경우
+			//최신화
 			if (cal <= L && maxScore < score) {
 				maxScore = score;
 			}
 			return;
 		}
+		//이후 항목에 대해 선택/미선택 탐색
 		dfs(n+1,cal+arr[n][1] , score + arr[n][0]);
 		dfs(n+1,cal, score);		
 	}
 
 	static void solv() throws IOException {
-
+		//입력 처리
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		L = Integer.parseInt(st.nextToken());
 
+		//배열 초기화 및 입력
 		arr = new int[N][2];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -35,6 +41,7 @@ public class SWEA5215_햄버거_다이어트 {
 			arr[i][1] = Integer.parseInt(st.nextToken());
 		}
 
+		//탐색
 		dfs(0, 0, 0);
 
 	}
@@ -43,7 +50,9 @@ public class SWEA5215_햄버거_다이어트 {
 		st = new StringTokenizer(br.readLine());
 		int T = Integer.parseInt(st.nextToken());
 
+		//TC를 입력 받은 후 풀이를 위한 solv 함수 실행
 		for (int i = 1; i < T + 1; i++) {
+			// 최대 값이 0보다 작을 수 없으므로 0으로 초기화.
 			maxScore = 0;
 			solv();
 			System.out.printf("#%d %d\n", i, maxScore);
