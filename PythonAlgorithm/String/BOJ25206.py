192705f2d32eb25954a4204f0a42097e393fdd54
Han Ju Kim <hjkim2902@gmail.com>
2024-03-23 22:37:19 +0900

    [Python3] BOJ 25206 commit

--- PythonAlgorithm/String/BOJ25206.py
+++ PythonAlgorithm/String/BOJ25206.py
@@ -1,13 +1,19 @@
-rating = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F']
-grade = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0]
+#
+# Title: BOJ 25206 너의 평점은
+# Date: 24.03.22
+#
+import sys
+input = sys.stdin.readline
 
-total = 0	# 학점 총합을 담을 변수
-result = 0	# (학점 * 과목평점) 총합을 담을 변수
-for _ in range(20) :
+rating = ["A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "D+", "F"]
+grade = [4.5, 4.0, 3.5, 3.0, 2.5, 1.5, 1.0, 0]
+
+total = 0
+result = 0
+for _ in range(20):
     s, p, g = input().split()
     p = float(p)
-    if g != 'P' :	# 등급이 P인 과목은 계산 안함
+    if g != "P":
         total += p
         result += p * grade[rating.index(g)]
-
-print('%.6f' % (result / total))
+print("%.6f" % (result/total))
