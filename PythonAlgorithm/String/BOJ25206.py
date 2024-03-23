b39a56ea3f7b19b32bb77fced2dc7cd44e0bcfb3
Han Ju Kim <hjkim2902@gmail.com>
2024-03-23 22:57:51 +0900

    [Python3] BOJ 25206 commit

--- PythonAlgorithm/String/BOJ25206.py
+++ PythonAlgorithm/String/BOJ25206.py
@@ -5,8 +5,8 @@
 import sys
 input = sys.stdin.readline
 
-rating = ["A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "D+", "F"]
-grade = [4.5, 4.0, 3.5, 3.0, 2.5, 1.5, 1.0, 0]
+rating = ["A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F"]
+grade = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0]
 
 total = 0
 result = 0
