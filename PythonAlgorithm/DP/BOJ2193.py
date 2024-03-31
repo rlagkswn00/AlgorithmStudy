6508c1031e57629d01df0a2fdd3975b4338ed39f
Han Ju Kim <hjkim2902@gmail.com>
2024-03-31 23:20:09 +0900

    [Python3] BOJ 2193  commit

---
+++ PythonAlgorithm/DP/BOJ2193.py
@@ -0,0 +1,15 @@
+#
+# Title: BOJ 2193 이친수
+# Date: 24.03.31
+#
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0] * (90+1)
+dp[1] = 1
+dp[2] = 1
+
+for i in range(3,n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+print(dp[n])
