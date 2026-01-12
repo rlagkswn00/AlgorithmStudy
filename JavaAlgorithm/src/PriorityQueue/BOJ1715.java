feffd8e82cc9b586294b4dbcf608cd1e37b7621e
Han Ju Kim <hjkim2902@gmail.com>
2026-01-12 09:58:52 +0900

    boj 1715 commit

---
+++ JavaAlgorithm/src/PriorityQueue/BOJ1715.java
@@ -0,0 +1,29 @@
+package PriorityQueue;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ1715 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < n; i++) {
+            int num =  Integer.parseInt(br.readLine());
+            pq.add(num);
+        }
+
+        int total = 0;
+
+        while (pq.size() >= 2) {
+            long num1 = pq.poll();
+            long num2 = pq.poll();
+
+            total += num1 + num2;
+            pq.add(total);
+        }
+        System.out.println(total);
+    }
+}
